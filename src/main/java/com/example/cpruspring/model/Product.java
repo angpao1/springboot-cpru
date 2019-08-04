ba2b68e9625122821bd9d8e516d1b842f511f39a
angpao <angpao.kittimasak@gmail.com>
2019-08-04 11:17:18 +0700

    create product model

---
+++ src/main/java/com/example/cpruspring/model/Product.java
@@ -0,0 +1,49 @@
+package com.example.cpruspring.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "product")
+public class Product {
+
+    @Id
+    private String id;
+    private String productName;
+    private String category;
+    private String price;
+
+    public Product() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
