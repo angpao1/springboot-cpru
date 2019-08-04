f582583dd6bf954f3dbc2852d8ec3d778649eec7
angpao <angpao.kittimasak@gmail.com>
2019-08-04 11:40:42 +0700

    create product controller

---
+++ src/main/java/com/example/cpruspring/Controllers/ProductController.java
@@ -0,0 +1,48 @@
+package com.example.cpruspring.Controllers;
+
+import com.example.cpruspring.model.Product;
+import com.example.cpruspring.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/products")
+@RestController
+public class ProductController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping()
+    public Iterable<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable String id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
+    @PostMapping("/add")
+    public Product addProduct(@RequestBody Product product) {
+        Product saved = productRepository.save(product);
+        return saved;
+    }
+
+    @PutMapping("/{id}")
+    public Product updateProduct(@PathVariable String id, @RequestBody Product product) {
+        Product productDetail = productRepository.findById(id).orElse(null);
+        productDetail.setProductName(product.getProductName());
+        productDetail.setCategory(product.getCategory());
+        productDetail.setPrice(product.getPrice());
+        Product response = productRepository.save(productDetail);
+        return response;
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteProduct(@PathVariable String id) {
+        Product product = productRepository.findById(id).orElse(null);
+        productRepository.delete(product);
+        return "Delete Product";
+    }
+
+}
