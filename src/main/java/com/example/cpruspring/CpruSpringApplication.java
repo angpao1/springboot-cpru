919edcc02409f6ab9b43643f8781e547f693f08b
angpao <angpao.kittimasak@gmail.com>
2019-08-04 10:46:41 +0700

    create Project

---
+++ src/main/java/com/example/cpruspring/CpruSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.cpruspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpruSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CpruSpringApplication.class, args);
+    }
+
+}
